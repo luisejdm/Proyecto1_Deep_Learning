da782278880385b294142df67b603b4a8ba5b2f8
Tato <smontesdeoca.-@iteso.mx>
2026-02-24 22:28:42 -0600

    corrections to processing data

--- proyecto1_deep_learning/data_loader.py
+++ proyecto1_deep_learning/data_loader.py
@@ -7,7 +7,26 @@ def load_data():
 
 #this works if you run yout jupyter command at the level of "/Proyecto1_deep_learning" 
 def load_data_2():
-    x_train = np.load("./data/processed/x_train.npy")
-    x_test = np.load("./data/processed/x_test.npy")
+    try:
+        x_train = np.load("../data/processed/x_train.npy")
+        x_test = np.load("../data/processed/x_test.npy")
+        return x_train, x_test
+    except Exception as e:
+        x_train = np.load("./data/processed/x_train.npy")
+        x_test = np.load("./data/processed/x_test.npy")
+    return x_train, x_test
+
+
+# np.save("./data/processed/x_train_128.npy", x_train.astype("uint8"))
+# np.save("./data/processed/x_test_128.npy", x_test.astype("uint8"))
+# print("saved dataset in ../data/processed/")
+def load_data_3():
+    try:
+        x_train = np.load("../data/processed/x_train_128.npy")
+        x_test = np.load("../data/processed/x_test_128.npy")
+        return x_train, x_test
+    except Exception as e:
+        x_train = np.load("./data/processed/x_train_128.npy")
+        x_test = np.load("./data/processed/x_test_128.npy")
     return x_train, x_test
 
